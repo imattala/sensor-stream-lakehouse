527bfe1ff91ace76fb80c6efdf4ae151c0d8aff0
Ibrahim ATTALA <mohamed.ibrahim.attala@gmail.com>
2026-09-08 22:58:21 +0200

    M4: add Kafka sink table DDL for sensor_readings_dlq

--- flink-job/src/main/java/dev/ibrahim/sensors/ddl/kafka/SensorKafkaDDL.java
+++ flink-job/src/main/java/dev/ibrahim/sensors/ddl/kafka/SensorKafkaDDL.java
@@ -21,4 +21,21 @@ public class SensorKafkaDDL {
                 "  'avro-confluent.url' = '" + sr + "'\n" +
                 ")";
     }
+
+    public static String dlqSink(String bs, String sr) {
+        return "CREATE TABLE IF NOT EXISTS sensor_readings_dlq (\n" +
+                "  `reading_id` STRING,\n" +
+                "  `device_id`  STRING,\n" +
+                "  `metric`     STRING,\n" +
+                "  `value`      DOUBLE,\n" +
+                "  `unit`       STRING,\n" +
+                "  `timestamp`  TIMESTAMP(3)\n" +
+                ") WITH (\n" +
+                "  'connector' = 'kafka',\n" +
+                "  'topic' = 'sensor-readings-dlq',\n" +
+                "  'properties.bootstrap.servers' = '" + bs + "',\n" +
+                "  'format' = 'avro-confluent',\n" +
+                "  'avro-confluent.url' = '" + sr + "'\n" +
+                ")";
+    }
 }
